7ca37961254f8befb75e68d961f73a44f801a17c
JoshuaRevan <deaconst.jhon2389@gmail.com>
2024-07-29 15:27:35 -0600

    Conexion Sencilla

---
+++ Conexion SQL Server/Clases/Conexion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Conexion_SQL_Server.Clases
+{
+    internal class Conexion
+    {
+        SqlConnection conex = new SqlConnection();
+
+         static string servidor = "DESKTOP-SG8MU6B";
+         static string bd= "Supermercado";
+         /*static string usuario="se";
+         static string password= "admin12345";
+         static string puerto= "1433";*/
+
+        // instancia usada para conexiones sencillas
+        string cadenaConexion = $"Data Source={servidor};Initial Catalog={bd};Integrated Security=True;";
+
+        //en dado caso esta linea de codigo se puede usar en conexiones que requieran usuarios
+        //string cadenaConexion = $"Data Source={servidor},{puerto};Initial Catalog={bd};User ID={usuario};Password={password};";
+
+        public SqlConnection establecerConexion()
+        {
+            try
+            {
+                conex.ConnectionString= cadenaConexion;
+                conex.Open();
+                MessageBox.Show("Se Conecto correctamente a la base de datos");
+
+            }
+            catch(SqlException e)
+            {
+                MessageBox.Show("No se logro conectar a la base de datos " + e.ToString());
+            }
+            return conex;
+        }
+    }
+}
---
+++ Conexion SQL Server/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Conexion_SQL_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConexion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConexion
+            // 
+            this.btnConexion.Location = new System.Drawing.Point(34, 49);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(309, 68);
+            this.btnConexion.TabIndex = 0;
+            this.btnConexion.Text = "Conectar a SQL Server";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 171);
+            this.Controls.Add(this.btnConexion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConexion;
+    }
+}
+
